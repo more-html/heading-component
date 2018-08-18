f3cdca36ac71b3379b7623ce3076b3ea9389c638
wolframkriesing <wolfram+github@kriesing.de>
2018-08-18 16:58:06 +0200

    Implement a way too complex solution for just replacing one char, but I know the future :).

--- src/MoreHtmlHeading.spec.js
+++ src/MoreHtmlHeading.spec.js
@@ -1,11 +1,18 @@
 const {spec: describe, unit: it} = require('kavun');
 const assert = require('assert');
 
+const umlautsMap = {
+  'ü': 'ue',
+};
+const replaceWith = (s, replacements) => s.split('').map(char => char in replacements ? replacements[char] : char).join(''); 
+const handleSpecialChars = s =>
+  replaceWith(s, umlautsMap);
+
 const headlineToHash = headline => {
   const lowerCase = s => s.toLowerCase();
   const spaceToDash = s => s.replace(/\s/, '-');
   const hash = () => (+(headline.toString().split('').map(s => s.charCodeAt(0)).join(''))).toString(32).substr(0, 7);  
-  return spaceToDash(lowerCase(headline)) + `---${hash()}`;
+  return handleSpecialChars(spaceToDash(lowerCase(headline))) + `---${hash()}`;
 };
 
 describe('Build hash from the headline text', () => {
@@ -17,4 +24,8 @@ describe('Build hash from the headline text', () => {
     const headline = 'Should RESULT in the same hash every time ...';
     assert.equal(headlineToHash(headline), headlineToHash(headline));
   });
+  describe('special character handling', () => {
+    it('umlaut `ü` gets extended to `ue` (for ze germans)', () => assert(headlineToHash('ü').startsWith('ue')));
+  });
 });
+
