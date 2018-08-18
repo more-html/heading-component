68b6fc228c7e9a8967a68d92e4f0903c8145f1c6
wolframkriesing <wolfram+github@kriesing.de>
2018-08-18 12:12:32 +0200

    Implement first version of headline hashing.

--- src/MoreHtmlHeading.spec.js
+++ src/MoreHtmlHeading.spec.js
@@ -1,9 +1,20 @@
 const {spec: describe, unit: it} = require('kavun');
 const assert = require('assert');
 
+const headlineToHash = headline => {
+  const lowerCase = s => s.toLowerCase();
+  const spaceToDash = s => s.replace(/\s/, '-');
+  const hash = () => (+(headline.toString().split('').map(s => s.charCodeAt(0)).join(''))).toString(32).substr(0, 7);  
+  return spaceToDash(lowerCase(headline)) + `---${hash()}`;
+};
+
 describe('Build hash from the headline text', () => {
   it('for "A Headline" generate all lower case with a hash at the end', () => {
     const headline = 'A Headline';
-    assert.equal(headlineToHash(headline), 'a-headline---8439ek');
+    assert.equal(headlineToHash(headline), 'a-headline---1m169ii');
+  });
+  it('the hash is deterministic, generates the same every time for the same string', () => {
+    const headline = 'Should RESULT in the same hash every time ...';
+    assert.equal(headlineToHash(headline), headlineToHash(headline));
   });
 });
