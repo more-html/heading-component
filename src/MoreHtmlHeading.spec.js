14c98401380c86ac75113cab4ab880c93dd43e20
wolframkriesing <wolfram+github@kriesing.de>
2018-08-18 17:45:32 +0200

    Use imports, remove code that had already moved out of test file.

--- src/MoreHtmlHeading.spec.js
+++ src/MoreHtmlHeading.spec.js
@@ -1,25 +1,6 @@
-const {spec: describe, unit: it} = require('kavun');
-const assert = require('assert');
-
-const umlautsMap = {
-  'ü': 'ue',
-  'ä': 'ae',
-  'ö': 'oe',
-  'ß': 'ss',
-  'ò': 'o',
-  'ñ': 'n',
-  'ì': 'i',
-};
-const replaceWith = (s, replacements) => s.split('').map(char => char in replacements ? replacements[char] : char).join(''); 
-const handleSpecialChars = s =>
-  replaceWith(s, umlautsMap);
-
-const headlineToHash = headline => {
-  const lowerCase = s => s.toLowerCase();
-  const spaceToDash = s => s.replace(/\s/, '-');
-  const hash = () => (+(headline.toString().split('').map(s => s.charCodeAt(0)).join(''))).toString(32).substr(0, 7);  
-  return handleSpecialChars(spaceToDash(lowerCase(headline))) + `---${hash()}`;
-};
+import {spec as describe, unit as it} from 'kavun';
+import assert from 'assert';
+import {headlineToHash} from './headline-to-hash.js';
 
 describe('Build hash from the headline text', () => {
   it('for "A Headline" generate all lower case with a hash at the end', () => {
