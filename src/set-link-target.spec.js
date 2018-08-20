389cbbdeb737e9ef4d56356b16423d06fd6c6867
wolframkriesing <wolfram+github@kriesing.de>
2018-08-20 19:32:49 +0200

    Moved it all to use a Map (interface). Makes it much simpler.

--- src/set-link-target.spec.js
+++ src/set-link-target.spec.js
@@ -1,23 +1,40 @@
 import {spec as describe, unit as it} from 'kavun';
 import assert from 'assert';
 
-// - existing ID must never be overridden
-// - given hash attribute and ID, ID overrules (maybe throw a warning!?)
-// - given hash and no ID, set hash as ID
-// - no hash, no ID, use generated hash as ID
+// - [x] existing ID must never be overridden
+// - [x] given hash attribute and ID, ID overrules 
+//   - [ ] (maybe throw a warning!?)
+// - [x] given hash and no ID, set hash as ID
+// - [ ] no hash, no ID, use generated hash as ID
 
-const setLinkTarget = () => {}
+const setLinkTarget = ({attributes}) => {
+  if (attributes.has('id')) return;
+  attributes.set('id', attributes.get('hash'));
+};
 
 describe('Set the link target for the hash', () => {
   it('GIVEN id attribute THEN dont override it', () => {
-    const readIdAttribute = () => 'id-attribute';
-    const readHashAttribute = () => undefined;
-    const setIdAttribute = () => { setIdAttribute.wasCalled = true; };
-    setIdAttribute.wasCalled = false;
-
-    setLinkTarget({readIdAttribute, readHashAttribute, setIdAttribute});
-
-    assert.equal(setIdAttribute.wasCalled, false);
+    const attributes = [['id', 'id attr']];
+    const attrMap = new Map(attributes);
+    setLinkTarget({attributes: attrMap});
+    assert.deepEqual(attrMap, new Map(attributes));
+  });
+  it('GIVEN hash+id attribute THEN dont override the ID', () => {
+    const attributes = [
+      ['id', 'id attr'],
+      ['hash', 'hash attr'],
+    ];
+    const attrMap = new Map(attributes);
+    setLinkTarget({attributes: attrMap});
+    assert.deepEqual(attrMap, new Map(attributes));
+  });
+  it('GIVEN hash attribute THEN set ID', () => {
+    const attributes = [
+      ['hash', 'hash attr'],
+    ];
+    const attrMap = new Map(attributes);
+    setLinkTarget({attributes: attrMap});
+    assert.deepEqual(attrMap, new Map([...attributes, ['id', 'hash attr']]));
   });
 });
 
