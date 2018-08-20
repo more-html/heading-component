13730a8dd0e1cf5b9c218020d1fdb154b5bef287
wolframkriesing <wolfram+github@kriesing.de>
2018-08-20 17:42:42 +0200

    Replace many dashes with one.

--- src/MoreHtmlHeading.spec.js
+++ src/MoreHtmlHeading.spec.js
@@ -15,6 +15,10 @@ describe('Build hash from the headline text', () => {
     const headline = 'a{b}c?d:e!f#g%h&i*j';
     assert(headlineToHash(headline).startsWith('a-b-c-d-e-f-g-h-i-j'));
   });
+  it('replace multiple dashes with one', () => {
+    const headline = 'abc----def--ghi-jkl';
+    assert(headlineToHash(headline).startsWith('abc-def-ghi-jkl'));
+  });
   it('the hash is deterministic, generates the same every time for the same string', () => {
     const headline = 'Should RESULT in the same hash every time ...';
     assert.equal(headlineToHash(headline), headlineToHash(headline));
--- src/headline-to-hash.js
+++ src/headline-to-hash.js
@@ -8,10 +8,11 @@ const umlautsMap = {
   'ñ': 'n',
   'ì': 'i',
 };
+const singlefyDashes = s => s.replace(/-{1,}/g, '-');
 const replaceNonAsciis = s => s.replace(/[^a-zA-Z0-9]/g, '-')
 const replaceWith = (s, replacements) => s.split('').map(char => char in replacements ? replacements[char] : char).join(''); 
 const handleSpecialChars = s =>
-  replaceNonAsciis(replaceWith(s, umlautsMap));
+  singlefyDashes(replaceNonAsciis(replaceWith(s, umlautsMap)));
 
 export const headlineToHash = headline => {
   const lowerCase = s => s.toLowerCase();
