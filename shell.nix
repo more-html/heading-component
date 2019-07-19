80c000386457908178bc3099055e87ba0d0292d5
wolframkriesing <wolfram+github@kriesing.de>
2019-07-19 20:50:09 +0200

    Upgrade to newer node.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
-with import (fetchTarball https://github.com/nixos/nixpkgs/tarball/151afd245708bbd441ce374b1661064862843739) { };
+with import (fetchTarball https://github.com/nixos/nixpkgs/tarball/525f0b043652cb62192874054d7aca5843d45e8f) { };
 
 stdenv.mkDerivation {
     name = "dev-shell";
     src = null;
-    buildInputs = [ nodejs-9_x ];
+    buildInputs = [ nodejs-12_x ];
 }
